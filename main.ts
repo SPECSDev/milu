0831a2f1f476c81c8e38beb232165d3ea71ab74f
SPECSDev <armin.duff@gmail.com>
2020-07-02 21:17:56 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
+//% groups=['LED matrix', 'Control flow', 'others']
+namespace basic {
+    //% block
+export function showMilo(v: number, interval: number = 150): void
+{
+
+ }
+}
 basic.forever(function () {
     servos.P0.setAngle(90)
     basic.showNumber(0)
